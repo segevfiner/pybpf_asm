aaada473db9a0ba5c4bd17d8d422d643b6591974
Segev Finer <segev208@gmail.com>
2022-02-06 11:22:55 +0200

    Fix error and add dumps

--- bpf_asm/_bpf_asm.pyx
+++ bpf_asm/_bpf_asm.pyx
@@ -1,11 +1,19 @@
 # cython: language_level=3str
 
+import enum
 from libc.stdlib cimport free
 from libc.stdint cimport uint8_t, uint16_t, uint32_t
 
 
 class Error(Exception):
     pass
+
+
+
+class BpfDumpType(enum.IntEnum):
+    DEFAULT = 0
+    MULTILINE = 1
+    C_ARRAY = 2
 
 
 cdef extern from "bpf_exp.yacc.h":
@@ -18,12 +26,13 @@ cdef extern from "bpf_exp.yacc.h":
     int bpf_asm_compile(const char *str, int len, sock_filter (**out), const char **error)
 
 
-def assemble(str):
+def assemble(str: str) -> [(int, int, int, int)]:
+    """Assemble BPF *str*."""
     cdef sock_filter *out = NULL
     cdef char *error = NULL
 
-    str = str.encode()
-    result = bpf_asm_compile(str, len(str), &out, &error)
+    strbytes = str.encode()
+    result = bpf_asm_compile(strbytes, len(strbytes), &out, &error)
     try:
         if not result:
             raise Error(error.decode())
@@ -35,3 +44,30 @@ def assemble(str):
     finally:
         free(out)
         free(error)
+
+
+def dumps(bpf: [(int, int, int, int)], format=BpfDumpType.DEFAULT) -> str:
+    """Dump *bpf* to a str in *format*."""
+    result = []
+
+    if format == BpfDumpType.DEFAULT:
+        result.append(f"{len(bpf)},")
+
+        for insn in bpf[:-2]:
+            result.append(f"{insn[0]} {insn[1]} {insn[2]} {insn[3]},")
+
+        insn = bpf[-1]
+        result.append(f"{insn[0]} {insn[1]} {insn[2]} {insn[3]}")
+
+        return ''.join(result)
+
+    elif format == BpfDumpType.MULTILINE:
+        result.append(f"{len(bpf)}")
+        for insn in bpf:
+            result.append(f"{insn[0]} {insn[1]} {insn[2]} {insn[3]}")
+        return '\n'.join(result)
+
+    elif format == BpfDumpType.C_ARRAY:
+        for insn in bpf:
+            result.append(f"{{ 0x{insn[0]:x} {insn[1]} {insn[2]} 0x{insn.k[3]:08x} }},")
+        return '\n'.join(result)
