4cf431e1f72841909084c12dc9de893c4d1ca83e
Segev Finer <segev208@gmail.com>
2022-11-04 00:09:38 +0200

    Support Python 3.11

--- setup.py
+++ setup.py
@@ -52,6 +52,7 @@ setup(
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
     ],
     keywords="bpf",
     zip_safe=False,
